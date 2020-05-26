ab964d0a532518dce7bf31da15e5524341ce3d02
Daniel Huber <C45981@launcher.int.abnamro.com>
2020-05-26 18:33:40 +0200

    extension added

--- CopyScriptUsingMD5.py
+++ CopyScriptUsingMD5.py
@@ -2,10 +2,12 @@ import os, sys
 import shutil
 import hashlib
 
-dir_src = ("C:/Users/xxxxxxx/temp_input_folder/")
+dir_src = ("C:/Users/xxxxx/temp_input_folder/")
 dir_dest = ("C:/Users/xxxxxx/temp_output_folder/")
+extension = ("txt")
 mydict = {}
 doublefilesdict = {}
+
 
 # genereer md5 hash waarde voor file in path, met gelimiteerd tot 64k buffer.
 def hashfile(path, blocksize = 65536):
@@ -25,21 +27,23 @@ def CheckDestinationFolder():
     for dirName, subdirs, fileList in os.walk(dir_dest):
         print('Scanning %s...' % dirName)
         for filename in fileList:
-            # Wat is het path, waar de file in staat, maak daar samen 1 string van.
-            path = os.path.join(dirName, filename)
-            # Bereken hash
-            file_hash = hashfile(path)
-            # voeg md5 hash waarde toe aan dictionary.
-            if file_hash in mydict:
-                if file_hash in mydict_double_destination:
-                    mydict_double_destination[file_hash].append(path)
-                    print ( "extra!Dubbele MD5 hash gevonden, in mydict_double_destination geplaatst = ", file_hash + path )
+            # alleen uitvoeren als files een bepaalde extensie hebben.
+            if filename.endswith(extension):
+                # Wat is het path, waar de file in staat, maak daar samen 1 string van.
+                path = os.path.join(dirName, filename)
+                # Bereken hash
+                file_hash = hashfile(path)
+                # voeg md5 hash waarde toe aan dictionary.
+                if file_hash in mydict:
+                    if file_hash in mydict_double_destination:
+                        mydict_double_destination[file_hash].append(path)
+                        print ( "extra!Dubbele MD5 hash gevonden, in mydict_double_destination geplaatst = ", file_hash + path )
+                    else:
+                        mydict_double_destination[file_hash] = [path]
+                        print ( "Dubbele MD5 hash gevonden, in mydict_double_destination geplaatst = ", file_hash + path )
                 else:
-                    mydict_double_destination[file_hash] = [path]
-                    print ( "Dubbele MD5 hash gevonden, in mydict_double_destination geplaatst = ", file_hash + path )
-            else:
-                mydict[file_hash] = [path]
-                print ( "initieel laden van MD5+filename in dictionary mydict = ", file_hash + path )
+                    mydict[file_hash] = [path]
+                    print ( "initieel laden van MD5+filename in dictionary mydict = ", file_hash + path )
     print( "================= Klaar met maken Destination Dictionary =====================" )
 
 # controleer Source op files, en valideer deze tegen bestaande files in mydict (dictionary) en kopieer.
@@ -48,32 +52,34 @@ def CopySourceFiles():
     for dirName, subdirs, fileList in os.walk(dir_src):
         print('Scanning %s...' % dirName)
         for filename in fileList:
-            # Wat is het path, waar de file in staat, maak daar samen 1 string van.
-            path = os.path.join(dirName, filename)
-            # Bereken hash
-            file_hash = hashfile(path)
-            # Als MD5 has waarde voorkomt in dictionary, plaats dan in dubbele dictionary. Anders kopieer bestand naar Destination en voeg md5 waarde toe aan dictionary.
-            if file_hash in mydict:
-                if file_hash in doublefilesdict:
-                    doublefilesdict[file_hash].append(path)
-                    print ( "extra!Dubbele MD5 hash gevonden, in doublefilesdict geplaatst = ", file_hash + path )
+            # alleen uitvoeren als files een bepaalde extensie hebben.
+            if filename.endswith(extension):
+                # Wat is het path, waar de file in staat, maak daar samen 1 string van.
+                path = os.path.join(dirName, filename)
+                # Bereken hash
+                file_hash = hashfile(path)
+                # Als MD5 has waarde voorkomt in dictionary, plaats dan in dubbele dictionary. Anders kopieer bestand naar Destination en voeg md5 waarde toe aan dictionary.
+                if file_hash in mydict:
+                    if file_hash in doublefilesdict:
+                        doublefilesdict[file_hash].append(path)
+                        print ( "extra!Dubbele MD5 hash gevonden, in doublefilesdict geplaatst = ", file_hash + path )
+                    else:
+                        doublefilesdict[file_hash] = [path]
+                        print ( "Dubbele MD5 hash gevonden, in doublefilesdict geplaatst = ", file_hash + path )
                 else:
-                    doublefilesdict[file_hash] = [path]
-                    print ( "Dubbele MD5 hash gevonden, in doublefilesdict geplaatst = ", file_hash + path )
-            else:
-                try:
-                    shutil.copy( path, dir_dest)                        
-                except IOError as e:
-                    print("Unable to copy file. %s" % e)
-                except:
-                    print("Unexpected error:", sys.exc_info())
-                                        
-                print ( "Bestand gekopieerd = ", dir_dest + filename )
-                
-                # voeg MD5 waarde van gekopieerde bestand toe.
-                mydict[file_hash] = [path]
-                
-                print ( "MD5+filename ge update in dictionary mydict = ", file_hash + path )
+                    try:
+                        shutil.copy( path, dir_dest)                        
+                    except IOError as e:
+                        print("Unable to copy file. %s" % e)
+                    except:
+                        print("Unexpected error:", sys.exc_info())
+                                            
+                    print ( "Bestand gekopieerd = ", dir_dest + filename )
+                    
+                    # voeg MD5 waarde van gekopieerde bestand toe.
+                    mydict[file_hash] = [path]
+                    
+                    print ( "MD5+filename ge update in dictionary mydict = ", file_hash + path )
 
     print( "================= Klaar met kopieren" )
 
